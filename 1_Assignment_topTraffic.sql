5cc1e3ec0d4da06c1a8e5fb2436ee4f0e1809b6b
Luan <42214331+Krytan@users.noreply.github.com>
2022-08-19 10:18:46 +0200

    added files

---
+++ 1_Assignment_topTraffic.sql
@@ -0,0 +1,29 @@
+
+USE mavenfuzzyfactory;
+
+SELECT utm_source,
+		utm_campaign,
+        http_referer,
+        COUNT(DISTINCT website_sessions.website_session_id) AS sessions
+FROM website_sessions
+WHERE website_sessions.created_at < '2012-04-12'
+GROUP BY 1,2,3
+ORDER BY 4 DESC
+
+
+
+/*
+OUTPUT
+
+utm_source  utm_campaign  http_referer          sessions
+
+'gsearch','nonbrand','https://www.gsearch.com', '3613'
+NULL,NULL,NULL,                                 '28'
+NULL,NULL,'https://www.gsearch.com',            '27'
+'gsearch','brand','https://www.gsearch.com',    '26'
+NULL,NULL,'https://www.bsearch.com',            '7'
+'bsearch','brand','https://www.bsearch.com',    '7'
+
+Top traffic is gsearch 
+*/
+
