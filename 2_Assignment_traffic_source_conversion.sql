cec110b25b737ab8313435a08fcbd068240846b4
Luan <42214331+Krytan@users.noreply.github.com>
2022-08-19 12:50:13 +0200

    more comments and fix readme

--- 2_Assignment_traffic_source_conversion.sql
+++ 2_Assignment_traffic_source_conversion.sql
@@ -1,3 +1,16 @@
+
+/*
+
+OUTPUT 
+
+"sessions" : 3895,
+"orders" : 112,
+"session_to_order_conv_rate" : 0.0288
+
+Traffic Source Conversion is 2.88% porcent which is below the 4% threshold the company needs to make.
+that minds the company is over-spending based on this conversion rate. 
+*/
+
 USE mavenfuzzyfactory;
 
 SELECT COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
@@ -14,14 +27,3 @@ WHERE website_sessions.created_at < '2012-04-14'
     AND utm_campaign = 'nonbrand'
 
 
-/*
-
-OUTPUT 
-
-"sessions" : 3895,
-"orders" : 112,
-"session_to_order_conv_rate" : 0.0288
-
-Traffic Source Conversion is 2.88% porcent which is below the 4% threshold the company needs to make.
-that minds the company is over-spending based on this conversion rate. 
-*/
